935844ef32aa29d29421c5a9bec4dff14503e22f
AnchisaPhetnoi <65030289@kmitl.ac.th>
2024-09-13 00:36:45 +0700

    Lab-07-Seven-segment-Component-develope

--- components/SevenSegment/main.cpp
+++ components/SevenSegment/main.cpp
@@ -1,25 +1,39 @@
 #include <stdio.h>
 #include "SevenSegment.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
-SevenSegment s1(0);  // Seven Segment ตัวแรก
-SevenSegment s2(4);  // Seven Segment ตัวที่สอง
+SevenSegment s1(0);
+SevenSegment s2(4);
 
-extern "C" void app_main(void)
+// create counter as global variable
+uint8_t counter = 0;
+
+// create task handle for LED display task
+TaskHandle_t xSevenSegmentHandle = NULL;
+
+// create task and move code into it
+void vTaskScanSevenSegment(void *Parameters)
 {
     while (1)
     {
-        // แสดงเลข 1 บน Seven Segment ตัวแรก
-        s1.DisplayNumber(1);
+        s1.DisplayNumber(counter / 10);
         s1.DisplayOn();
-        vTaskDelay(500 / portTICK_PERIOD_MS); // หน่วงเวลา 500ms
+        vTaskDelay(10 / portTICK_PERIOD_MS);
+        s1.DisplayOff();
 
-        s1.DisplayOff(); // ปิดการแสดงผลบน Seven Segment ตัวแรก
-
-        // แสดงเลข 5 บน Seven Segment ตัวที่สอง
-        s2.DisplayNumber(5);
+        s2.DisplayNumber(counter % 10);
         s2.DisplayOn();
-        vTaskDelay(500 / portTICK_PERIOD_MS); // หน่วงเวลา 500ms
-
-        s2.DisplayOff(); // ปิดการแสดงผลบน Seven Segment ตัวที่สอง
+        vTaskDelay(10 / portTICK_PERIOD_MS);
+        s2.DisplayOff();
     }
+}
+
+
+// in main, create display task
+extern "C" void app_main(void)
+{
+    xTaskCreate(vTaskScanSevenSegment, 
+      "Seven Seg", 1024, NULL, 
+      tskIDLE_PRIORITY, &xSevenSegmentHandle);
 }
