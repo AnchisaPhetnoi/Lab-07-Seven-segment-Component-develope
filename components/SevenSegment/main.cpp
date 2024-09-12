c5095a11c096954e844e1ce72c6daf99de0faeb8
AnchisaPhetnoi <65030289@kmitl.ac.th>
2024-09-12 23:41:46 +0700

    Lab-07-Seven-segment-Component-develope

--- components/SevenSegment/main.cpp
+++ components/SevenSegment/main.cpp
@@ -1,30 +1,23 @@
 #include <stdio.h>
 #include "SevenSegment.h"
 
-SevenSegment s1(0);
-SevenSegment s2(4);
+SevenSegment s1(0);  // Seven-Segment Display สำหรับหลักสิบ
+SevenSegment s2(4);  // Seven-Segment Display สำหรับหลักหน่วย
 
 extern "C" void app_main(void)
 {
-    uint8_t counter = 0;  // ใช้ตัวแปร counter แบบ uint8_t
-    
+    uint8_t counter = 15;  // กำหนดค่าเริ่มต้นให้เป็น 15
+
     while (1)
     {
-        // แสดงหลักสิบบน Seven-Segment Display ที่ 1
+        // แสดงหลักสิบ (เลข 1) บน Seven-Segment Display ที่ 1
         s1.DisplayNumber(counter / 10);
         s1.DisplayOn();
-        vTaskDelay(500 / portTICK_PERIOD_MS);  // แสดงผลเป็นเวลา 500 ms
-        s1.DisplayOff();
 
-        // แสดงหลักหน่วยบน Seven-Segment Display ที่ 2
+        // แสดงหลักหน่วย (เลข 5) บน Seven-Segment Display ที่ 2
         s2.DisplayNumber(counter % 10);
         s2.DisplayOn();
-        vTaskDelay(500 / portTICK_PERIOD_MS);  // แสดงผลเป็นเวลา 500 ms
-        s2.DisplayOff();
 
-        // เพิ่มค่า counter
-        counter++;
-        if (counter > 99)
-            counter = 0;
+        vTaskDelay(1000 / portTICK_PERIOD_MS);  // ให้แสดงผลค้างไว้ 1 วินาที
     }
 }
